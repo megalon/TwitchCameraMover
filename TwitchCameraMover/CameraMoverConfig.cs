02cded27462718b28adfe6d71f7f0e886f2ef5f8
xspeedasx <xspeedasx@gmail.com>
2019-03-07 08:42:08 +0200

    Added multiple slide actions queuing Changed cam to use transform instead Changed lerping to linear motion instead of infinite traveling to destination.
    added queue method to !cam which chains few slides together to make cinematic approach.
    Increased max duration to 300s.
    Increased max camera distance to 1000
    Implemented easing functions (easeinout and bezier) for smoother sliding
    
    configurable lookAt
    configurable permissions

---
+++ TwitchCameraMover/CameraMoverConfig.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchCameraMover
+{
+    public class CameraMoverConfig
+    {
+        public CameraMoverConfig()
+        {
+            MaxCameraDist = 1000;
+            CommandsRestricted = true;
+            AllowForMods = true;
+            AllowForSubs = true;
+            AllowForVips = true;
+            LookAt = new UnityEngine.Vector3(0, 1, 0);
+
+            var filePath = Path.Combine(Environment.CurrentDirectory, "UserData", "TwitchCameraMover.cnf");
+            if (!File.Exists(filePath))
+            {
+
+                var txt = "";
+                foreach (var fi in typeof(CameraMoverConfig).GetFields())
+                {
+                    txt += fi.Name + "=" + fi.GetValue(this) + "\r\n";
+                }
+
+                File.WriteAllText(filePath, txt);
+                return;
+            }
+
+            var contents = File.ReadAllText(filePath);
+            var lines = contents.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None);
+
+            //temp lazy workaround without using a parser
+            foreach (var ln in lines)
+            {
+                var line = ln.Trim();
+                var eqidx = line.IndexOf('=');
+                if (eqidx == -1) continue;
+
+                var key = line.Substring(0, eqidx);
+                var prop = typeof(CameraMoverConfig).GetProperty(key);
+                if (prop == null) continue;
+
+                var val = line.Substring(Math.Min(eqidx + 1, line.Length));
+
+                if (prop.Name == "LookAt")
+                {
+                    var splits = val.Split(' ');
+                    if (splits.Length != 3) continue;
+
+                    var vec = new UnityEngine.Vector3(float.Parse(splits[0]), float.Parse(splits[1]), float.Parse(splits[2]));
+                    prop.SetValue(this, vec);
+                    continue;
+                }
+                switch (Type.GetTypeCode(prop.PropertyType))
+                {
+                    case TypeCode.Boolean:
+                        prop.SetValue(this, val.ToLower() == "true");
+                        break;
+                    case TypeCode.Single:
+                    case TypeCode.Double:
+                        prop.SetValue(this, float.Parse(val));
+                        break;
+                    case TypeCode.Int32:
+                        prop.SetValue(this, int.Parse(val));
+                        break;
+                    case TypeCode.String:
+                        prop.SetValue(this, val);
+                        break;
+                    default:
+                        prop.SetValue(this, prop.InvokeConstructor(val));
+                        break;
+                }
+            }
+        }
+
+        public float MaxCameraDist { get; set; }
+        public bool CommandsRestricted { get; set; }
+        public UnityEngine.Vector3 LookAt { get; set; }
+        public bool AllowForMods { get; set; }
+        public bool AllowForVips { get; set; }
+        public bool AllowForSubs { get; set; }
+
+    }
+
+    public static class CamMovExtensions
+    {
+        public static string ToString(this UnityEngine.Vector3 vec) => $"{vec.x} {vec.y} {vec.z}";
+    }
+}
---
+++ TwitchCameraMover/SlideAction.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TwitchCameraMover
+{
+    public class SlideAction
+    {
+        public enum SlideMotionType { Linear, Bezier, EaseInOut };
+
+        public Vector3 StartPos { get; set; }
+        public Vector3 TargetPos { get; set; }
+        public float Progress { get; set; }
+        public float Duration { get; set; } // duration in seconds for slide to complete
+        public SlideMotionType SlideMotion { get; set; }
+
+        public SlideAction()
+        {
+            Duration = 1;
+            Progress = 0;
+            SlideMotion = SlideMotionType.Linear;
+        }
+
+        public SlideAction(Vector3 start, Vector3 target, float duration, SlideMotionType slideMotionType = SlideMotionType.Linear)
+        {
+            this.StartPos = start;
+            this.TargetPos = target;
+            this.Duration = duration;
+            this.Progress = 0;
+            this.SlideMotion = slideMotionType;
+        }
+
+        public Vector3 MoveAndGetPos(float deltaTime, bool easeInOut)
+        {
+            if (Duration <= 0) return TargetPos;
+
+            var deltaProgress = deltaTime / Duration;
+            if (Progress + deltaProgress > 1) return TargetPos;
+            Progress += deltaProgress;
+
+            if (easeInOut)
+            {
+                float sqt = Progress * Progress;
+                //float bezierProg = sqt * (3 - 2 * Progress);
+                float prog = sqt / (2.0f * (sqt - Progress) + 1.0f);
+                return Vector3.Lerp(StartPos, TargetPos, prog);
+            }
+            else
+            {
+                return Vector3.Lerp(StartPos, TargetPos, Progress);
+            }
+
+        }
+        public float UpdateProgress(float deltaTime)
+        {
+            if (Duration <= 0) return 1;
+
+            var deltaProgress = deltaTime / Duration;
+            if (deltaProgress <= 1)
+                Progress += deltaProgress;
+
+            return deltaProgress;
+        }
+
+        public Vector3 GetPos(float? progress = null, SlideMotionType slideMotionType = SlideMotionType.Linear)
+        {
+            var prog = progress == null ? Progress : progress.Value;
+            float sqt = prog * prog;
+
+            switch (slideMotionType)
+            {
+                case SlideMotionType.Bezier:
+                    // less curvy
+                    // bezier func: x^2(3-2x)     https://www.wolframalpha.com/input/?i=x%5E2(3-2x)+from+0+to+1
+                    return Vector3.Lerp(StartPos, TargetPos, sqt * (3 - 2 * prog));
+                case SlideMotionType.EaseInOut:
+                    // more curvy
+                    // easeinout func: x^2/(2*(x^2 -x)+1)   https://www.wolframalpha.com/input/?i=x%5E2%2F(2*(x%5E2+-x)%2B1)+from+0+to+1
+                    return Vector3.Lerp(StartPos, TargetPos, sqt / (2.0f * (sqt - prog) + 1.0f));
+                default:
+                    return Vector3.Lerp(StartPos, TargetPos, prog);
+            }
+        }
+    }
+}
